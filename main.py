b01baa1d96320bf41144c3ad6a96eaf0ef7c5740
Fredy Santos <fredy0113@gmail.com>
2025-05-27 23:14:05 -0400

    Main Prompt Endpoints Completed

--- main.py
+++ main.py
@@ -38,12 +38,21 @@ def get_all_admin_prompts():
     )
     return response["Items"]
 
+
 def get_all_user_prompts(user_id):
     response = prompts_table.query(
         IndexName="UserIndex",
         KeyConditionExpression=Key("user_id").eq(user_id)
     )
     return response["Items"]
+
+
+def get_specific_prompt(prompt_id):
+    response = prompts_table.get_item(
+        Key={"prompt_id": prompt_id}
+    )
+
+    return response["Item"] #Will return None if not found
 
 
 # CREATE PROMPT
@@ -57,6 +66,11 @@ def create_prompt(prompt_text, level, user_id):
         'public': True
     })
     return prompt_id
+
+
+# Delete Prompts
+def delete_prompt_by_id(prompt_id):
+    prompts_table.delete_item(Key={'prompt_id': prompt_id})
 
 
 @app.route("/")
@@ -89,13 +103,51 @@ def me():
 # --------------------
 
 
-
+# Obtains either all prompts or filters prompts to obtain all prompts of the same level
 @app.route("/prompts", methods=["GET"])
-def get_prompts():
-    # Scan table and return items
-    pass
+def get_all_prompts():
+    user_id = get_user_id_from_request()
+
+    admin_prompts = get_all_admin_prompts()
+    user_prompts = get_all_user_prompts(user_id)
+    all_prompts = admin_prompts + user_prompts
+
+    if request.args.get("level"):
+        filtered_prompts = []
+        for prompt in all_prompts:
+            if prompt["level"].lower() == request.args.get("level").lower():
+                filtered_prompts.append(prompt)
+        return jsonify(filtered_prompts), 200
+    else:
+        return jsonify(all_prompts["text"]), 200
 
 
+# Obtains a specific prompt from a specific level
+@app.route("/prompts/random", methods=["GET"])
+def get_random_prompt():
+    level = request.args.get("level")
+    #First check if level is provided, otherwise provide error
+    if not level:
+        return jsonify({"error": "Missing level query parameter"}), 400
+
+    user_id = get_user_id_from_request()
+
+    admin_prompts = get_all_admin_prompts()
+    user_prompts = get_all_user_prompts(user_id)
+    all_prompts = admin_prompts + user_prompts
+
+    filtered_prompts = []
+
+    for prompt in all_prompts:
+            if prompt["level"].lower() == level.lower():
+                filtered_prompts.append(prompt)
+
+    prompt = random.choice(filtered_prompts)
+
+    return jsonify(prompt), 200
+
+
+# Add a prompt
 @app.route("/prompts", methods=["POST"])
 def add_prompt():
     data = request.json
@@ -108,10 +160,21 @@ def add_prompt():
     return jsonify({"prompt_id": prompt_id}), 201
 
 
+# Delete prompt by ID from DynamoDB
 @app.route("/prompts/<id>", methods=["DELETE"])
 def delete_prompt(id):
-    # Delete prompt by ID from DynamoDB
-    pass
+    prompt_to_delete = get_specific_prompt(id)
+
+    if not prompt_to_delete:
+        return jsonify({"error": "Prompt doesn't exist"}), 404
+    
+    user_id = get_user_id_from_request()
+
+    if user_id != prompt_to_delete["user_id"]:
+        return jsonify({"error": "User doesn't have permission"}), 403
+    
+    delete_prompt_by_id(id)
+    return jsonify({"message": f"Success: Prompt {id} deleted"}), 200
 
 
 # --------------------
